81e8736e1156f05b8a0e431177bf8cee3787660b
thiaguera00 <thiagocaetano155@gmail.com>
2023-10-23 16:14:30 -0300

    talwind configurado no projeto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
 </head>
 <body>
-    
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </body>
 </html>
