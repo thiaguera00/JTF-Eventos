96914953406c3b0317696b669a16f84fee694b4e
thiaguera00 <thiagocaetano155@gmail.com>
2024-05-25 10:30:16 -0300

    removido comentarios da controller

--- app/Http/Controllers/AcessoAoSistemaController.php
+++ app/Http/Controllers/AcessoAoSistemaController.php
@@ -14,19 +14,17 @@ class AcessoAoSistemaController extends Controller
             'senha' => 'required'
         ]);
 
-        // Busca o registro de acesso pelo login fornecido
+       
         $acesso = AcessoAoSistema::where('login', $request->login)->first();
-        // dd(AcessoAoSistema::where('login', $request->login)->first());
-        // Verifica se o usuário existe e se a senha está correta
+        
         if ($acesso && password_verify($request->senha, $acesso->senha)) {
-            // Autentica o usuário
+           
             Auth::login($acesso);
 
-            // Redireciona o usuário para a rota desejada após o login
+            
             return redirect()->intended(route('eventos'));
         }
 
-        // Redireciona de volta para a página de login com uma mensagem de erro
         return redirect()->route('login')->withErrors(['login' => 'Credenciais inválidas']);
     }
 }
